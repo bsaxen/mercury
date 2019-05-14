f2c38437861d446dd140cbae824dcd4e0f539ee5
bsaxen <benny.saxen@gmail.com>
2019-05-14 17:30:16 +0200

    Create application.php

---
+++ application.php
@@ -0,0 +1,9 @@
+<?php
+//=============================================
+// File.......: application.php
+// Date.......: 2019-05-14
+// Author.....: Benny Saxen
+// Description: Application Manager
+//=============================================
+
+?>
