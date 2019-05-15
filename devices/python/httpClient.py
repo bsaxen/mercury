3b10fa90e11da647f0336161fbfc8383d912cf1b
bsaxen <benny.saxen@gmail.com>
2019-05-15 16:26:37 +0200

    Create httpClient.py

---
+++ devices/python/httpClient.py
@@ -0,0 +1,35 @@
+# =============================================
+# File: httpClient.py
+# Author: Benny Saxen
+# Date: 2019-05-15
+# Description: Mercury Client
+# =============================================
+from iotLib import *
+#===================================================
+# Setup
+#===================================================
+confile = "httpclient.conf"
+version = 1
+lib_setup(co,confile,version)
+#===================================================
+# Loop
+#===================================================
+while True:
+    lib_increaseMyCounter(co,dy)
+
+    msg = lib_publishMyMeta(co,dy)
+
+    payload = '{"test":"10043","test2": "453"}'
+    print payload
+    msg = lib_publishMyPayload(co,dy,payload)
+
+    lib_commonAction(co,msg)
+
+    message = 'counter:' + str(dy.mycounter)
+    lib_publishMyLog(co, message)
+
+    print "sleep: " + str(co.myperiod) + " triggered: " + str(dy.mycounter)
+    time.sleep(float(co.myperiod))
+#===================================================
+# End of file
+#===================================================
