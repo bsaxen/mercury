b021b9b458afbc8b46e5c5585e063064d2562a07
bsaxen <benny.saxen@gmail.com>
2019-05-16 12:31:56 +0200

    Delete application.php

--- application.php
+++
@@ -1,9 +0,0 @@
-<?php
-//=============================================
-// File.......: application.php
-// Date.......: 2019-05-14
-// Author.....: Benny Saxen
-// Description: Application Manager
-//=============================================
-
-?>
